6aa0961cc4a51eaaf56c989f474bbd6c67575cb3
Elio Gerges <Elio-Gerges@outlook.com>
2025-01-10 01:13:48 +0200

    feat: adding google analytics container

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    ignorePatterns: ["!./components/*", "!./app/*"],
+    ignorePatterns: ["!./components/*", "!./app/*", "./app/layout.tsx"],
   },
 ];
 
